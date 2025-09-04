d09b0d6178b4ebbab3c9e932a38160c731d827c8
Samridhi Rauthan <100908675+Samoon19@users.noreply.github.com>
2025-09-04 10:32:31 +0530

    Update program1.c

--- chall3/program1.c
+++ chall3/program1.c
@@ -11,7 +11,7 @@ const char *data1 = "array1: ACNCTF{ur_og_flag}";
 const char *data2 = "array2: X9z1@#%&LMNOP";
 
 // real flag: 5-bit chunks (kept as raw text so it shows in .rdata)
-const char *data3 = "array3: 01001 10111 00110 10011 10010 10001 00001 01011 10010 10110 10111";
+const char *data3 = "array3: 01001 10110 10111 00110 10101 01011 10010 10001 00001 01011 10010 00100 10110 10111 10011";
 
 // fake #3: more random bits
 const char *data4 = "array4: 10101 01010 11100 00101 11011 01101";
@@ -34,3 +34,4 @@ int main() {
     helper();
     return 0;
 }
+
